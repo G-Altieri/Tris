a3f3d2b4fa168db53de7cd17888fd28e00013768
Giovanni Altieri <giovanni.alt1@gmail.com>
2022-04-02 01:55:54 +0200

    Add plane with 9block and add place object on click

--- pages/tris3d.vue
+++ pages/tris3d.vue
@@ -6,6 +6,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import Stats from "three/examples/jsm/libs/stats.module";
+import { Interaction } from "three.interaction/src/index.js";
 
 export default {
   name: "tris3D",
@@ -125,8 +126,6 @@ export default {
       let camera, scene, raycaster, renderer;
 
       let INTERSECTED;
-      let theta = 0;
-
       var objIntersected = [];
 
       //Pointer for mouse movement
@@ -138,16 +137,22 @@ export default {
 
       function init() {
         //Camera
-        camera = new THREE.PerspectiveCamera(
+        /* camera = new THREE.PerspectiveCamera(
           70,
           window.innerWidth / window.innerHeight,
           1,
           10000
+        );*/
+        camera = new THREE.PerspectiveCamera(
+          30,
+          window.innerWidth / window.innerHeight,
+          0.1,
+          5000
         );
 
         //Scene
         scene = new THREE.Scene();
-        scene.background = new THREE.Color(0xf0f0f0);
+        scene.background = new THREE.Color(0x000000);
 
         //Light
         const light = new THREE.DirectionalLight(0xffffff, 1);
@@ -156,30 +161,56 @@ export default {
 
         //Helper
         const gridHelper = new THREE.GridHelper(200, 50);
-        scene.add(gridHelper);
+        //scene.add(gridHelper);
 
         //Plane
-        /*const geometry = new THREE.BoxGeometry(20, 20, 20);
-
-         const object = new THREE.Mesh(
-          geometry,
-          new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff })
-        );
-        scene.add(object);
-        objIntersected.push(object);*/
         const geometryCube = new THREE.BoxGeometry(1, 0.1, 1);
-        const materialOne = new THREE.MeshLambertMaterial({ color: 0xffffed00 });
-        const materialTwo = new THREE.MeshLambertMaterial({ color: 0xff0ff4c6 });
-        const cube1 = new THREE.Mesh(geometryCube, materialOne);
-        const cube2 = new THREE.Mesh(geometryCube, materialTwo);
-        cube2.position.x = -1;
-        scene.add(cube1, cube2);
+
+        const materialOne = new THREE.MeshLambertMaterial({
+          color: 0xffffff,
+        });
+        const materialTwo = new THREE.MeshLambertMaterial({
+          color: 0xff0ff4c6,
+        });
+        let k = 0;
+        for (let i = 0; i < 3; i++) {
+          for (let j = 0; j < 3; j++) {
+            const materialRandom = new THREE.MeshLambertMaterial({
+              color: Math.random() * 0xffffff,
+            });
+            const object = new THREE.Mesh(geometryCube, materialRandom);
+            k += 1;
+            object.position.x = j;
+            object.position.y = 0;
+            object.position.z = i;
+            object.cursor = "pointer";
+            object.name = k;
+            object.on("click", function (ev) {
+              console.log(ev);
+              console.log("Click obg " + ev.data.target.name);
+              position(ev.data.target.name);
+            });
+
+            scene.add(object);
+            objIntersected.push(object);
+          }
+        }
+
+        /* const cube1 = new THREE.Mesh(geometryCube, materialTwo);
+        cube1.position.x = -1;
+        scene.add(cube1);
         objIntersected.push(cube1);
-        objIntersected.push(cube2);
+         cube1.cursor = "pointer";
+        cube1.on("click", function (ev) {
+          console.log(objIntersected);
+          console.log("Click 1");
+        });*/
+        
+        //Camera Position
         camera.position.z = 0;
         camera.position.x = 0;
-        camera.position.y = 0;
-        
+        camera.position.y = 10;
+
         //RayCaster declaration
         raycaster = new THREE.Raycaster();
 
@@ -194,12 +225,15 @@ export default {
         //Controls
         const controls = new OrbitControls(camera, renderer.domElement);
         controls.enableZoom = true;
-        controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
-        controls.dampingFactor = 0.05;
+        // controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+        // controls.dampingFactor = 0.05;
 
         //Stats for info
         stats = new Stats();
         container.appendChild(stats.dom);
+
+        //Interaction
+        const interaction = new Interaction(renderer, scene, camera);
 
         //Listener for mouse movement
         document.addEventListener("mousemove", onPointerMove);
@@ -221,6 +255,49 @@ export default {
         pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
       }
 
+      //Method for place X or O
+      function position(k) {
+        const geometryCube = new THREE.CylinderGeometry(0.5, 1, 2, 32);
+        const materialRandom = new THREE.MeshLambertMaterial({
+          color: Math.random() * 0xffffff,
+        });
+        const object = new THREE.Mesh(geometryCube, materialRandom);
+        object.scale.x = 0.1;
+        object.scale.z = 0.1;
+        object.scale.y = 0.1;
+        console.log("k " + k);
+        var position = findPosition(k);
+        console.log("Position " + position);
+        object.position.y = 0.2;
+        object.position.x = position[0];
+        object.position.z = position[1];
+        scene.add(object);
+      }
+
+      //Method Usufull for game logic
+      function findPosition(k) {
+        switch (k) {
+          case 1:
+            return [0, 0];
+          case 2:
+            return [1, 0];
+          case 3:
+            return [2, 0];
+          case 4:
+            return [0, 1];
+          case 5:
+            return [1, 1];
+          case 6:
+            return [2, 1];
+          case 7:
+            return [0, 2];
+          case 8:
+            return [1, 2];
+          case 9:
+            return [2, 2];
+        }
+      }
+
       //Animate Loop
       function animate() {
         requestAnimationFrame(animate);
@@ -232,14 +309,12 @@ export default {
 
       //Render Loop
       function render() {
-        //theta += 0.1;
-
         //camera.position.x = radius * Math.sin(THREE.MathUtils.degToRad(theta));
         //  camera.position.y = radius * Math.sin(THREE.MathUtils.degToRad(theta));
         //camera.position.z = radius * Math.cos(THREE.MathUtils.degToRad(theta));
         //camera.lookAt(scene.position);
 
-        //   camera.updateMatrixWorld();
+        camera.updateMatrixWorld();
 
         // Find intersections
         raycaster.setFromCamera(pointer, camera);
@@ -265,8 +340,9 @@ export default {
 
         //ReRender
         renderer.render(scene, camera);
-       // controls.update();
+        // controls.update();
       }
+      //Other Methods
     },
   },
   components: {},
