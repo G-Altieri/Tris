adecc8a83587007b136a01509589d752e5cdc5e2
Giovanni Altieri <giovanni.alt1@gmail.com>
2022-04-15 02:36:52 +0200

    add favicon and change color

---
+++ pages/index_beta.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <TRIS3D />
+</div>
+  <!-- <div class="min-h-screen bgTris">
+    <div class="text-6xl md:text-7xl lg:text-9xl font-medium text-white flex justify-center">TRIS</div>
+   <div class="flex flex-col items-center justify-center">
+     
+    <NuxtLink
+      to="/tris2d"
+      class="text-white text-center mt-20 border-4 border-lime-500 rounded-lg w-32"
+      >TRIS 2D</NuxtLink
+    >
+    <NuxtLink
+      to="/tris3d"
+      class="text-white text-center mt-20 border-4 border-lime-500 rounded-lg w-32"
+      >TRIS 3D</NuxtLink
+    >
+   </div>
+  </div> -->
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
+
+<style>
+.bgTris {
+  background-color: #050401;
+}
+</style>
